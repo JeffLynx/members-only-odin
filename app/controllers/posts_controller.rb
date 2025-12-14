baa67c74bad4f3cf2f18e223faa6bc38cb08dd0a
JeffLynx <55367715+JeffLynx@users.noreply.github.com>
2025-12-14 23:44:37 +0100

    adds Post model+controller and associations+routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to @post, notice: "Post created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+    private
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+end
